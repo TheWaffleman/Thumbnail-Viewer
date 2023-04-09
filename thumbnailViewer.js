d2712925dc0d82e7e487700de4094f4a64fa2cce
TheWaffleman <t1tanw4ff3l@gmail.com>
2023-04-09 03:15:52 +0200

    refactoring/bugfixing

--- thumbnailViewer.js
+++ thumbnailViewer.js
@@ -1,48 +1,36 @@
-browser.pageAction.onClicked.addListener(getURL);
+browser.browserAction.onClicked.addListener(openThumbnailInNewTab);
 
-function getURL() {
-
-  browser.tabs.query({currentWindow: true, active:true}).then(queryInfo => {
-    browser.tabs.get(queryInfo[0].id).then(tab => {
-      var tabUrl = tab.url;
-      
-      openTab("https://i.ytimg.com/vi/" + YouTubeGetID(tabUrl) +"/maxresdefault.jpg", tabUrl);
-         
+function openThumbnailInNewTab() {
+  browser.tabs
+    .query({ currentWindow: true, active: true })
+    .then((queryInfo) => {
+      browser.tabs.get(queryInfo[0].id).then((tab) => {
+        if (tab.url.includes("youtube.com", 0)) openTab(getYouTubeID(tab.url));
+      });
     });
-  });
 }
 
-function openTab(url,url1){   
+function openTab(youtubeID) {
   const img = new Image();
-  var urlold = "https://i.ytimg.com/vi/" + YouTubeGetID(url1) +"/hqdefault.jpg";
-  img.addEventListener("load", function() {
-    if(img.height >= 120){
+  let url = "https://i.ytimg.com/vi/" + youtubeID + "/maxresdefault.jpg";
+  img.src = url;
+
+  img.addEventListener("load", function () {
+    if (img.height >= 120) {
       createTabs(url);
     } else {
-      createTabs(urlold);
+      createTabs("https://i.ytimg.com/vi/" + youtubeID + "/hqdefault.jpg");
     }
   });
-  img.src = url;
 }
 
-function createTabs(url1){
+function createTabs(url) {
   browser.tabs.create({
-    url: url1
+    url: url,
   });
 }
 
-function YouTubeGetID(url){
-   url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-   return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
+function getYouTubeID(url) {
+  url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  return url[2] !== undefined ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
 }
-
-function checkForValidUrl(tabId, changeInfo, tab) {
-  // If the tabs url starts with "http://specificsite.com"...
-  if (tab.url.indexOf('https://www.youtube.com') == 0) {
-      // ... show the page action.
-      browser.pageAction.show(tabId);
-  }
-};
-
-// Listen for any changes to the URL of any tab.
-browser.tabs.onUpdated.addListener(checkForValidUrl);
